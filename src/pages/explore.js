2f08b7e4c51cc3c5156b5b8d3ae43d5281edc1c3
Kristin Baumann <mail@kristin-baumann.com>
2019-06-01 20:38:49 +0200

    Added contentful connection

--- src/pages/explore.js
+++ src/pages/explore.js
@@ -1,11 +1,35 @@
 import React from "react"
+import { graphql } from "gatsby"
 
 import Layout from "../components/layout"
 
-const PageExplore = props => (
-  <Layout pathname={props.location.pathname}>
-    <p>Page Explore</p>
-  </Layout>
-)
+const PageExplore = props => {
+  const posts = props.data.allContentfulPost.edges
+  return (
+    <Layout pathname={props.location.pathname} style={{}}>
+      <div style={{ color: "black" }}>
+        <p>Page Explore</p>
+        <ul>
+          {posts.map(({ node }) => (
+            <li key={node.id}>{node.headline}</li>
+          ))}
+        </ul>
+      </div>
+    </Layout>
+  )
+}
 
 export default PageExplore
+
+export const pageQuery = graphql`
+  query PostQuery {
+    allContentfulPost {
+      edges {
+        node {
+          id
+          headline
+        }
+      }
+    }
+  }
+`
