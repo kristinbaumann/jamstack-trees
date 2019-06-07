bc95fba441fe6b0017e54d6a1fb9300c0135d632
Kristin Baumann <mail@kristin-baumann.com>
2019-06-07 15:30:27 +0200

    Removed unnecessary modules

--- gatsby-config.js
+++ gatsby-config.js
@@ -4,38 +4,17 @@ if (process.env.NODE_ENV === "development" || !process.env.NODE_ENV) {
 
 module.exports = {
   plugins: [
-    `gatsby-plugin-react-helmet`,
+    "gatsby-plugin-react-helmet",
     "gatsby-plugin-stripe",
     {
-      resolve: `gatsby-source-stripe`,
+      resolve: "gatsby-source-stripe",
       options: {
         objects: ["Sku"],
         secretKey: process.env.STRIPE_SECRET_KEY,
       },
     },
     {
-      resolve: `gatsby-source-filesystem`,
-      options: {
-        name: `images`,
-        path: `${__dirname}/src/images`,
-      },
-    },
-    `gatsby-transformer-sharp`,
-    `gatsby-plugin-sharp`,
-    {
-      resolve: `gatsby-plugin-manifest`,
-      options: {
-        name: `gatsby-starter-default`,
-        short_name: `starter`,
-        start_url: `/`,
-        background_color: `#663399`,
-        theme_color: `#663399`,
-        display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
-      },
-    },
-    {
-      resolve: `gatsby-source-contentful`,
+      resolve: "gatsby-source-contentful",
       options: {
         spaceId: process.env.CONTENTFUL_SPACE_ID,
         accessToken: process.env.CONTENTFUL_ACCESS_TOKEN,
