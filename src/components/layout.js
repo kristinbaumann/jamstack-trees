48d5e139674f2c88bace5893643be8f30e05c020
Kristin Baumann <mail@kristin-baumann.com>
2019-06-07 16:07:45 +0200

    Renamed component

--- src/components/layout.js
+++ src/components/layout.js
@@ -2,8 +2,8 @@ import React from "react"
 import PropTypes from "prop-types"
 import { Helmet } from "react-helmet"
 
-import Header from "./Header"
-import Footer from "./Footer"
+import Header from "./header"
+import Footer from "./footer"
 import "../styles/layout.css"
 import "../styles/styles.css"
 
